77480f3f91d86e4ee0a2321d524e69d6d6f3da0a
Rushikesh Chaudhari <rushi.chaudhari@yahoo.com>
2020-12-24 12:17:41 +0000

    function with if else

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function hello(){
+readline = require("readline-sync");
+
+function isFive(num){
   console.log("Hello World");
+  if(num==5){
+    console.log("The number is 5");
+  }
+  else{
+    console.log("The number is not 5");
+  }
 }
 
-hello();
+const n= readline.question("Enter number : ");
+isFive(n);
+
