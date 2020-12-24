0a4aa3c53f52bd5177eda3cf887a45644899ca71
Rushikesh Chaudhari <rushi.chaudhari@yahoo.com>
2020-12-24 18:48:07 +0000

    odd even

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 rSync = require("readline-sync");
 
-function show(str){
-  console.log(str);
+// function show(str){
+//   if(str == "rushi"){
+//     console.log("welcome! Master")
+//   }
+//   else{
+//     console.log("get out from here")
+//   }
+// }
+
+// let name = rSync.question("Enter your name : ")
+// show(name)
+
+let num = 12;
+if(num % 2 == 0){
+  console.log("Even")
 }
-
-let name = rSync.question("Enter your name : ")
-show(name);
-
+else{
+  console.log("Odd");
+}
